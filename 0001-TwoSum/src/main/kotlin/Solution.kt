ffb7d64a23b2cab45ab375dbb8ac273a2b9b27a8
Tolga Çobanoğlu <76070986+tolgacobanoglu@users.noreply.github.com>
2022-06-06 15:47:33 +0300

    Add files via upload

---
+++ 0001-TwoSum/src/main/kotlin/Solution.kt
@@ -0,0 +1,41 @@
+//05.06.2022
+
+fun main()
+{
+    //Example 1:
+    var numbers = intArrayOf(2,7,11,15)
+    var target = 9
+
+    println(twoSum(numbers,target).contentToString())
+
+    //Example 2:
+    numbers = intArrayOf(3,2,4)
+    target = 6
+
+    println(twoSum(numbers,target).contentToString())
+
+
+    //Example 3:
+    numbers = intArrayOf(3,3)
+    target = 6
+
+    println(twoSum(numbers,target).contentToString())
+
+}
+
+
+fun twoSum(numbers: IntArray, target: Int): IntArray {
+
+    for (i in 0..numbers.size-1)
+    {
+        for (j in 1..numbers.size-1)
+        {
+            if (numbers[i] + numbers[j] == target)
+            {
+                return intArrayOf(i , j)
+            }
+        }
+    }
+
+    return intArrayOf(-1,-1) // for error condition
+}
